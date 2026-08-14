f12109b588275b264203f288568eeb27575eb67c
vishalkalva <kalvavishal26@gmail.com>
2026-08-14 15:20:25 +0530

    rock paper scissors two
    with score and play again

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -20,3 +20,44 @@ elif (user == "rock" and computer == "scissors") or\
 
 else:
     print("computer won!")
+
+
+
+# with score and play again 
+import random
+
+choice = ("rock", "paper", "scissors")
+
+user_score = 0
+computer_score = 0
+
+while True:
+
+    user = input("Enter rock, paper, scissors: ").lower()
+
+    computer = random.choice(choice)
+
+    print("Your choice:", user)
+    print("Computer choice:", computer)
+
+    if user == computer:
+        print("It's a tie")
+
+    elif (user == "rock" and computer == "scissors") or \
+         (user == "paper" and computer == "rock") or \
+         (user == "scissors" and computer == "paper"):
+        print("You won!")
+        user_score += 1
+
+    else:
+        print("Computer won!")
+        computer_score += 1
+
+    print("Score - You:", user_score, "| Computer:", computer_score)
+
+    again = input("Play again? (yes/no): ").lower()
+
+    if again == "no":
+        print("Final Score - You:", user_score, "| Computer:", computer_score)
+        print("Thanks for playing!")
+        break
